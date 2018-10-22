05164eff7f8d71525a31417d4677712cd7a24226
fadikamohamed <fadika.mohamed02@gmail.com>
2018-10-22 09:13:04 +0200

    exercice terminé

---
+++ exercice3.sql
@@ -0,0 +1,4 @@
+-- Connexion a la base de donnée --
+mysql -u root -p
+-- Création d'une bdd frameworks encodé en utf8 si elle n'existe pas--
+CREATE DATABASE IF NOT EXISTS `frameworks` CHARACTER SET `utf8`;
